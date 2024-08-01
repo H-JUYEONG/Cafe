1793e908329310b2a4ce0e12708f561d3b708e1f
H-JUYEONG <dud9902@gmail.com>
2024-08-01 10:18:14 +0900

    ㅇ

--- selectTables.sql
+++ selectTables.sql
@@ -28,7 +28,8 @@ where receipt_id = 4
 ;
 
 -- 판매된 상품명, 수량, 음료의 개당가격
-select d.drink_name,        -- 음료 이름
+select u.user_id, -- 회원번호
+	   d.drink_name,        -- 음료 이름
        sum(uo.drink_cnt) AS drink_cnt, -- 총 판매된 수량
        d.drink_price,      -- 개당 가격
        sum(uo.drink_cnt * d.drink_price) AS sum_price, -- 음료마다 가격 총합
@@ -39,8 +40,11 @@ inner join Drink d
 on uo.drink_id = d.drink_id  -- 음료 정보를 가져오기 위해 Drink 테이블과 조인
 inner join Receipt r
 on uo.receipt_id = r.receipt_id
+inner join Users u 
+on r.user_id = u.user_id
+
 where date_format(r.receipt_date, '%Y-%m-%d') = '2024-07-31'
-group by d.drink_name, d.drink_price, r.receipt_date
+group by d.drink_name, d.drink_price, r.receipt_date, u.user_id
 
 ;
 
